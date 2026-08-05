abae0ef7b0f553fa6e85f11659cb17eac69aa232
Yangtao Lin <44685186+YangLin320@users.noreply.github.com>
2026-08-05 17:27:34 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if(s.length != t.length) return false
+
+        const sMap = new Map<string, number>();
+        const tMap = new Map<string, number>();
+
+        for(let i = 0; i < s.length; i++){
+            sMap.set(s[i], (sMap.get(s[i]) ?? 0) + 1);
+            tMap.set(t[i], (tMap.get(t[i]) ?? 0)+ 1);
+        }
+
+        for(const key of sMap.keys()){
+            if(sMap.get(key) != tMap.get(key)) return false;
+        }
+
+        return true;
+    }
+}
