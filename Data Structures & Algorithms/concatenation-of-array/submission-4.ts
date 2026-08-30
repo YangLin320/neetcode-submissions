af1a30400e9744b8575395e812bf5d01cd731197
Yangtao Lin <44685186+YangLin320@users.noreply.github.com>
2026-08-30 11:00:22 -0400

    Add: concatenation-of-array - submission-4

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-4.ts
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums: number[]): number[] {
+        let ans: number[] = [];
+
+        for(let x: number=0; x < nums.length; x++){
+            ans[x] = ans[x + nums.length] = nums[x];
+        }
+        return ans;
+    }
+}
